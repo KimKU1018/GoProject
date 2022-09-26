a71b76af6c6c06074c81a4903c6db857c230810a
KimKU1018 <86593582+KimKU1018@users.noreply.github.com>
2022-09-26 13:48:41 +0900

    상수에 대한 공부

--- hello.go
+++ hello.go
@@ -2,7 +2,16 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello World!")
+func Divide(a, b int) (int, bool) {
+	if b == 0 {
+		return 0, false
+	}
+	return a / b, true
+}
 
+func main() {
+	c, success := Divide(9, 3)
+	fmt.Println(c, success)
+	d, success := Divide(9, 0)
+	fmt.Println(d, success)
 }
